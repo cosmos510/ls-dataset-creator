b239b51fbcaa4c2ba30e1bacca77ca9d744dbc3a
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2024-11-24 16:43:28 +0100

    test in prod without metadata

--- app/api/photo/timer/route.js
+++ app/api/photo/timer/route.js
@@ -38,6 +38,7 @@ export async function POST(req) {
       }
 
       // Insert metadata for this batch
+      /*
       const { data: insertData, error: insertError } = await supabase
         .from('photo')
         .insert([
@@ -54,7 +55,7 @@ export async function POST(req) {
         return NextResponse.json({ error: 'Error saving metadata' }, { status: 500 });
       }
 
-      uploadedImages.push(insertData);
+      uploadedImages.push(insertData);*/
     }
 
     return NextResponse.json({
