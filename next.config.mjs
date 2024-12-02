dc7281b001848197bbdb38efd29a11dfe6363e96
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2024-12-02 09:24:07 +0100

    back to normal for the next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' www.googletagmanager.com www.google-analytics.com;
-  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
-  img-src 'self' data:;
-  font-src 'self' fonts.googleapis.com fonts.gstatic.com;
-  connect-src 'self' www.google-analytics.com ;
-  frame-ancestors 'none';
-  object-src 'none';
-  base-uri 'self';
-`;
-
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
